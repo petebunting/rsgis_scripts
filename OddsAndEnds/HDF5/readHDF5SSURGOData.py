99881ba38d0cea7d93f1acc3ecb815c51dafd727
Daniel Clewley <4453490+danclewley@users.noreply.github.com>
2012-08-25 09:12:54 -0700

    Added more detailed comments in code to convert SSURGO HDF5 files to GeoTiff (kea is much better suited as an output format as also HDF5 and compressed but changed to GeoTiff for compatibility)

--- OddsAndEnds/HDF5/readHDF5SSURGOData.py
+++ OddsAndEnds/HDF5/readHDF5SSURGOData.py
@@ -5,22 +5,26 @@
 # Lines for calculating band statistics taken from:
 # http://www.jeremymsmith.us/davidson/NDVI.py
 
+# Import required libraries
 import sys
 import h5py, gdal, osr
 import numpy as np
 
-inHDF5File = sys.argv[1] #'/home/danclewley/Documents/Temp/ssurgo_tonzi/ca067.h5'
-outGEOTiff = sys.argv[2] #'/home/danclewley/Documents/Temp/ssurgo_tonzi/ca067_tif.tif'
-
-inputLayers = ['K_o','K_sat','L_parameter','N_parameter','alpha','bulk_density_0.33bar', 'depth_to_bottom','depth_to_top','horizon_thickness','percent_clay','percent_sand','percent_silt','theta_res','theta_sat','water_retention_0.33bar','water_retention_15bar']
-
-numBands = len(inputLayers)
+# Get input and output files from the command line or hardcode
+inHDF5File = sys.argv[1] #'/data/ssurgo_tonzi/ca067.h5'
+outGEOTiff = sys.argv[2] #'/data/ca067_tif.tif'
 
 # Read in data
 soildata = h5py.File(inHDF5File)
 
-# List available datasets
+# Set input layers available in HDF5 file, found by running:
 #list(soildata)
+# in interactive python mode after opening the HDF5 file.
+
+inputLayers = ['K_o','K_sat','L_parameter','N_parameter','alpha','bulk_density_0.33bar', 'depth_to_bottom','depth_to_top','horizon_thickness','percent_clay','percent_sand','percent_silt','theta_res','theta_sat','water_retention_0.33bar','water_retention_15bar']
+
+# Set the number of bands (for the output image)
+numBands = len(inputLayers)
 
 # Get geospatial information
 geoTransform = []
@@ -42,11 +46,12 @@ geoTransform[3] = maxLat # top left y
 geoTransform[4] = 0
 geoTransform[5] = (minLat - maxLat) / float(inYSize)
 
+# Set projection to WGS84 - Lat / Long
 srs = osr.SpatialReference()
 srs.SetWellKnownGeogCS("WGS84")
 
 # Creat output image
-driver = gdal.GetDriverByName("KEA")
+driver = gdal.GetDriverByName("GTiff") # Could put any gdal supported layers here
 metadata = driver.GetMetadata()
 newDataset = driver.Create(outGEOTiff, inXSize, inYSize, numBands, gdal.GDT_Float32)
 newDataset.SetGeoTransform(geoTransform)
@@ -62,14 +67,17 @@ for band in range(numBands):
     for line in soildata[layerName]:
         outLine = []
         for pixel in line:
+            # For each pixel there are 6 layers, I assumed only the first was actual data and the others were metadata
+            # You can to pixel[required band] to alter this 
             if pixel[0] == -9999:
                 outLine.append(0.)
             else:
                 outLine.append(pixel[0])
         outArray.append(outLine)
-
+    
+    # Set output as numpy array (so it can be transposed if required)
     outArray = np.array(outArray)
-    if outArray.shape[0] != inYSize:
+    if outArray.shape[0] != inYSize: # Some data I had the lat/long and data arrays were rotated so it was neccesary to transpose
         outArray = np.transpose(outArray)
 
     newDataset.GetRasterBand(band+1).WriteArray(outArray)
@@ -79,7 +87,5 @@ for band in range(numBands):
     # Set output name
     newDataset.GetRasterBand(band+1).SetDescription(layerName)
 
-# Build overview images
-newDataset.BuildOverviews()
 # Close dataset
 newDataset = None
