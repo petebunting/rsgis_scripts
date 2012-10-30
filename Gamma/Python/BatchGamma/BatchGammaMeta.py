8ca4524ad82c32394a91daaebe24fea637b3d260
Daniel Clewley <4453490+danclewley@users.noreply.github.com>
2012-10-30 09:18:26 -0700

    Added contact name and email as input parameter. Metadata now includes link to scripts and Earth Observation Group website.

--- Gamma/Python/BatchGamma/BatchGammaMeta.py
+++ Gamma/Python/BatchGamma/BatchGammaMeta.py
@@ -15,12 +15,11 @@ from datetime import date
 
 class BatchGammaMeta (object):
 
-    def __init__(self, sceneDIR, mode):
+    def __init__(self, sceneDIR, dataOwner, dataContact):
         self.sceneDIR = sceneDIR
         self.scenename = self.removeFilePathUNIX(sceneDIR)
-        self.mode = mode
-        self.dataOwner = "Richard Lucas / Dan Clewley"
-        self.dataContact = "rml@aber.ac.uk / ddc06@aber.ac.uk"
+        self.dataOwner = dataOwner
+        self.dataContact = dataContact
         d = date.today()
         self.importTime = str(d.strftime("%d/%m/%y"))
         
@@ -38,25 +37,25 @@ class BatchGammaMeta (object):
     def createMetaText(self):
         metatxt = '''
 %s
-ALOS PALSAR %s data
+ALOS PALSAR data
 
-Processed to Level 1.5 (georeffernced product)
-Using BatchGamma%s.py
-All SAR processing using GAMMA, with CSH scripts included
-in directory.
+Processed to Level 1.5 (georeffernced product) using scripts developed within the Earth Observation Group at Aberystwyth University (http://www.aber.ac.uk/en/iges/research-groups/earth-observation-laboratory/), and available to download from https://bitbucket.org/petebunting/rsgis_scripts .
+
+All SAR processing using GAMMA (http://www.gamma-rs.ch/), with CSH scripts included in directory.
 
 Products:
 *.utm - Georeffernced sigma0 data
 *.gamma.utm - Georefferenced gamma0 data
-*.topo.utm - Georefferenced topographicaly corrected data (Using the method of Castel et al. 2001)
+*.topo.utm - Georefferenced topographicaly corrected data (using the method of Castel et al. 2001)
 *.inc - Local incidence angle image
 *.pix - Local pixel area (relative to 'flat' projection of pixel)
+*.png - Quick look images (stretched)
 
 Process Date: %s
 Contact: %s
 Email: %s
         
-        ''' % (self.scenename, self.mode,  self.mode, self.importTime, self.dataOwner, self.dataContact)
+        ''' % (self.scenename, self.importTime, self.dataOwner, self.dataContact)
         
         return metatxt
         
@@ -68,5 +67,5 @@ Email: %s
         
 if __name__ == '__main__':
     # Test program
-    obj = BatchGammaMeta("/Users/danclewley/Desktop/Test/", "FBD")
+    obj = BatchGammaMeta("/home/danclewley/Desktop/", "Dan Clewley / Richard Lucas", "ddc06@aber.ac.uk")
     obj.writeMetaText()
