23290fdb9d8eb92da513c3e7edb8d8cad8fd91ed
Daniel Clewley <4453490+danclewley@users.noreply.github.com>
2013-01-29 22:03:48 -0800

    Added shell script to install some of the more general scripts to a specified directory.

---
+++ install.sh
@@ -0,0 +1,8 @@
+mkdir $1/bin
+cp ./General/BatchCommand/BatchCommand.py $1/bin/
+cp ./General/BatchCommand/BatchCommandPP.py $1/bin/
+cp ./Vector/OGR2OGR/CreateCSVfromSHP.py $1/bin/
+cp ./Text/FindReplaceTextDIR.py $1/bin/
+cp ./Text/JoinTablesCSV.py $1/bin/
+cp ./gdalrats/setthematic.py $1/bin/
+cp ./File/Rename/reFindandReplaceFileName.py $1/bin/
