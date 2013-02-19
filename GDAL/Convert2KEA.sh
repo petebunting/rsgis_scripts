1515c9387226ab2036a280e13520c79ea90cc247
Pete Bunting <6181851+petebunting@users.noreply.github.com>
2013-02-19 16:25:42 +0000

    Additional WKT files.

---
+++ GDAL/Convert2KEA.sh
@@ -0,0 +1,20 @@
+# Created by Pete Bunting (petebunting@mac.com)
+#
+# A simple script to use gdal_translate to convert files 
+# to the KEA format.
+#
+
+# Inputs:
+# $1 is the input directory 
+# $2 is the output directory
+# $3 is the input files extension
+
+FILES=$1/*.$3
+for f in $FILES
+do
+  echo "Processing $f file..."
+  filename=`basename ${f} .${3}`
+  echo "Output: ${2}/${filename}.kea"
+  gdal_translate -of KEA ${f} ${2}/${filename}.kea
+  gdalcalcstats ${2}/${filename}.kea -ignore 0
+done
