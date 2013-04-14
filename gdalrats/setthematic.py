1ff57afb30b357f4622e32cdd4d33292e25cdfb0
Daniel Clewley <4453490+danclewley@users.noreply.github.com>
2013-04-14 13:47:55 -0700

    Added help to set thematic

--- gdalrats/setthematic.py
+++ gdalrats/setthematic.py
@@ -3,6 +3,13 @@
 import sys
 from osgeo import gdal
 
+if len(sys.argv) < 2:
+	print '''Error: No image provided:
+Usage:
+	setthematic.py image.kea
+'''
+	exit()
+
 ds = gdal.Open(sys.argv[1], gdal.GA_Update)
 for bandnum in range(ds.RasterCount):
     band = ds.GetRasterBand(bandnum + 1)
