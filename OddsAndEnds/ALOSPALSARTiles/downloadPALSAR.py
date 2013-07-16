4b989595f31497320fc77eadeb2133df83328d0b
Daniel Clewley <4453490+danclewley@users.noreply.github.com>
2013-07-16 08:07:53 -0700

    Fixed some issues with script to download files using curl.

--- OddsAndEnds/ALOSPALSARTiles/downloadPALSAR.py
+++ OddsAndEnds/ALOSPALSARTiles/downloadPALSAR.py
@@ -24,17 +24,18 @@ class CurlDownload (object):
         command = ''
         fulloutputpath = ''
         for i in range(len(filenames)):
+            print('''Downloading file %i / %i'''%(i, len(filenames)))
             fulloutputpath = os.path.join(outputDIR, filenames[i])
             if os.path.exists(fulloutputpath):
                 print('Already downloaded ' + filenames[i])
             else:
-                command = curl + filenames[i] + ' > ' + fulloutputpath
+                command = curlCommand + filenames[i] + ' > ' + fulloutputpath
                 #print command
                 os.system(command)
             
     def run(self):
         numArgs = len(sys.argv)
-        if numArgs == 3:
+        if numArgs == 4:
             inputTextFilePath = sys.argv[1]
             outputDIR = sys.argv[2]
             curlCommand = sys.argv[3]
