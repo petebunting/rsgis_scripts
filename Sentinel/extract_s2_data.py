960ece3dcb4dfca6c60dff5cbe520dd827926f51
Daniel Clewley <4453490+danclewley@users.noreply.github.com>
2016-08-16 21:03:37 +0100

    Get UTM zone from filename.

--- Sentinel/extract_s2_data.py
+++ Sentinel/extract_s2_data.py
@@ -35,9 +35,17 @@ if gdal.__version__ < LooseVersion("2.1.0"):
     print("This script requires GDAL 2.1 or later", file=sys.stderr)
     sys.exit(1)
 
-def get_subdataset_names(infile):
+def get_subdataset_names(infile, resolution=None):
     """
     Get subdataset names from a Sentinel 2 file
+
+    Excludes preview images (last two subdatasets)
+
+    If 'resolution' is passed in will only return subdatasets matching this
+
+    Returns tuple from GDAL with:
+        (Name, Description)
+
     """
     # Check if a directory has been passed in (rather than xml)
     if os.path.isdir(infile):
@@ -53,57 +61,35 @@ def get_subdataset_names(infile):
 
     # Last two subdatasets are preview images so ignore these.
     for sub_data in subdatasets[:-2]:
-        out_names.append(sub_data[0])
+        if resolution is not None:
+            if resolution not in VALID_RESOLUTIONS:
+                raise Exception("Resolution {} is not valid. Available options "
+                                "are 10, 20 and 60".format(resolution))
+            # Get resolution
+            subdata_res = sub_data[0].split(":")[2].rstrip("m")
+            if int(subdata_res) == int(resolution):
+                out_names.append(sub_data)
+        else:
+            out_names.append(sub_data)
 
     return out_names
 
-def convert_epsg_utm(epsg_code):
-    """
-    Convert EPSG code into a string with UTM zone
-    """
-    spatial_ref = osr.SpatialReference()
-    osr_out = spatial_ref.ImportFromEPSG(int(epsg_code))
-
-    if osr_out != 0:
-        raise Exception("Could not create projection. "
-                        "Is {} a valid EPSG code".format(epsg_code))
-
-    utm_zone = spatial_ref.GetUTMZone()
-
-    if utm_zone != 0:
-        # If positive North
-        if utm_zone > 0:
-            utm_zone_str = str(utm_zone).zfill(2)
-            out_proj_str = "UTM%sN" % (utm_zone_str)
-        # If negative South
-        else:
-            utm_zone = utm_zone * -1
-            utm_zone_str = str(utm_zone).zfill(2)
-            out_proj_str = "UTM%sS" % (utm_zone_str)
-    else:
-        raise Exception("EPSG code does not corespond to a UTM zone")
-
-    return out_proj_str
-
-def get_out_name(subdataset_name, out_ext=""):
+def get_out_name(subdataset_name_tuple, out_ext=""):
     """
     Get output file name for sub dataset
+
+    Takes tuple with (subdataset name, description)
     """
+    subdataset_name = subdataset_name_tuple[0]
     outname = os.path.split(subdataset_name)[-1]
 
     outname = outname.replace(".xml","")
     outname = outname.replace(":","_")
 
-    # Try to convert EPSG code into easier to read UTM zone
-    epsg_code = outname.split("_")[-1]
-    try:
-        utm_str = convert_epsg_utm(epsg_code)
-        outname = "_".join(outname.split("_")[:-2])
-        outname = "{}_{}".format(outname, utm_str)
-    # If this fails go with EPSG code
-    except Exception as err:
-        print(err, file=sys.stderr)
-        pass
+    # Get UTM string from description.
+    utm_str = subdataset_name_tuple[1].split(",")[-1].replace(" ","")
+    outname = "_".join(outname.split("_")[:-2])
+    outname = "{}_{}".format(outname, utm_str)
 
     outname = outname + out_ext
 
