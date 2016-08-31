a9c5f0557c0fd305027b6a0bb5e0823c56373727
Daniel Clewley <4453490+danclewley@users.noreply.github.com>
2016-08-31 20:29:12 +0100

    Added ability to merge masks (vector format) for Sentinel-2.

--- Sentinel/extract_s2_data.py
+++ Sentinel/extract_s2_data.py
@@ -18,8 +18,8 @@ import sys
 import glob
 import subprocess
 from distutils.version import LooseVersion
+
 from osgeo import gdal
-from osgeo import osr
 
 # Try to import NERC-ARF 'get_gdal_drivers' library
 # Available from https://github.com/pmlrsg/arsf_dem_scripts/blob/master/arsf_dem/get_gdal_drivers.py
@@ -34,6 +34,12 @@ except ImportError:
 if gdal.__version__ < LooseVersion("2.1.0"):
     print("This script requires GDAL 2.1 or later", file=sys.stderr)
     sys.exit(1)
+
+VALID_RESOLUTIONS = [10, 20, 60]
+
+# QI dataset names
+QI_ALL_BANDS = ["CLOUDS"]
+QI_INDIVIDUAL_BANDS = ["DEFECT", "DETFOO", "NODATA", "SATURA", "TECQUA"]
 
 def get_subdataset_names(infile, resolution=None):
     """
@@ -95,18 +101,119 @@ def get_out_name(subdataset_name_tuple, out_ext=""):
 
     return outname
 
+def check_num_vector_layers(input_vector):
+    """
+    Gets the number of layers within a vector
+    """
+
+    ds = gdal.OpenEx(input_vector, gdal.OF_VECTOR)
+    num_layers = ds.GetLayerCount()
+    ds = None
+
+    return num_layers
+
+
+def merge_gml_to_sqlite(input_vector_files, output_vector, layer_name=None):
+    """
+    Merge multiple GML files to a single SQLite vector file.
+
+    Will remove output file if it already exists.
+    """
+    output_format = "SQLite"
+    if layer_name is None:
+        output_layer_name = os.path.splitext(os.path.basename(output_vector))[0]
+    else:
+        output_layer_name = layer_name
+    first_file = True
+    if os.path.isfile(output_vector):
+        os.remove(output_vector)
+
+    for i, input_vector in enumerate(input_vector_files):
+
+        if first_file:
+            # For first file need to create new output (will fail if already exists)
+            first_file = False
+            convert_cmd = ["ogr2ogr",
+                           "-f", output_format,
+                           "-nln", output_layer_name,
+                           output_vector, input_vector]
+            subprocess.check_call(convert_cmd)
+        else:
+            convert_cmd = ["ogr2ogr",
+                           "-update", "-append",
+                           "-nln", output_layer_name,
+                           output_vector, input_vector]
+            subprocess.check_call(convert_cmd)
+
+
+def merge_qi_vectors(input_dir, output_dir):
+    """
+    Merge all QI vectors from GML format for individual granules to a single
+    SQLite file
+    """
+
+    if os.path.splitext(input_dir)[0] == '.zip':
+        raise IOError("Need to unzip directory to extract QI data")
+
+    input_basename = os.path.basename(input_dir)
+    input_basename = input_basename.replace(".SAFE","")
+
+    for flag in QI_ALL_BANDS:
+        gml_files_list = glob.glob(os.path.join(input_dir, "GRANULE", "*", "QI_DATA",
+                                                "*MSK_{}*_B00*.gml".format(flag)))
+        output_file = os.path.join(output_dir,
+                                   "{}_{}_B00.sqlite".format(input_basename, flag))
+        if len(gml_files_list) == 0:
+            print("Could not find any files for '{}' flag".format(flag),
+                  file=sys.stderr)
+        else:
+            merge_gml_to_sqlite(gml_files_list, output_file, flag)
+            if check_num_vector_layers(output_file) == 0:
+                os.remove(output_file)
+            else:
+                print(" " + os.path.basename(output_file))
+
+    # Set up list of bands
+    bands_list = [str(i).zfill(2) for i in range(1,13)]
+    bands_list.append('8A')
+
+    for flag in QI_INDIVIDUAL_BANDS:
+        for band in bands_list:
+            gml_files_list = glob.glob(os.path.join(input_dir, "GRANULE",
+                                                     "*", "QI_DATA",
+                                                     "*MSK_{}*_B{}*.gml".format(flag, band)))
+            output_file = os.path.join(output_dir,
+                                "{}_{}_B{}.sqlite".format(input_basename, flag, band))
+            if len(gml_files_list) == 0:
+                print("Could not find any files for '{}' flag"
+                      "(band {})".format(flag, band),
+                      file=sys.stderr)
+            else:
+                merge_gml_to_sqlite(gml_files_list, output_file, flag)
+                if check_num_vector_layers(output_file) == 0:
+                    os.remove(output_file)
+                else:
+                    print(" " + os.path.basename(output_file))
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Optimise rasters for display "
-                                                 "by calculating statistics and adding "
-                                                 "overviews.")
+    parser = argparse.ArgumentParser(description="Extract Sentinel-2 data to"
+                                                 " indivitual files using GDAL")
     parser.add_argument("infiles",nargs="+",
-                        help="Input file(s)")
+                              help="Input file(s)")
     parser.add_argument("-o", "--outdir",
-                        help="Output directory",
-                        required=True)
-    parser.add_argument("--of", default="KEA",
-                        required=False,
-                        help="Output format (GDAL name)")
+                              help="Output directory",
+                              required=True)
+    parser.add_argument("--of", default="ENVI",
+                              required=False,
+                              help="Output format (GDAL name)")
+    parser.add_argument("--resolution", default=None,
+                              required=False,
+                              type=int,
+                              help="Resolution to extract. Options are 10, 20 and 60")
+    parser.add_argument("--extract_qi", default=False,
+                              required=False,
+                              action='store_true',
+                              help="Also extract QI masks to SQLite database")
     args = parser.parse_args()
 
     # Get extension and creation options
@@ -115,21 +222,22 @@ if __name__ == "__main__":
         out_creation_options = get_gdal_drivers.GDALDrivers().get_creation_options_from_ext(out_ext)
     # If get_gdal_drives is not available a couple of options are hardcoded
     else:
-        if args.of == "KEA":
-            out_ext = ".kea"
+        if args.of == "ENVI":
+            out_ext = ".bsq"
             out_creation_options = []
-        elif args.of == "GTiff":
-            out_ext = ".tif"
-            out_creation_options = ["COMPRESS=LZW"]
+        elif args.of == "netCDF":
+            out_ext = "nc"
+            out_creation_options = ['FORMAT=NC4C', 'COMPRESS=DEFLATE']
         else:
             print("Could not import 'get_gdal_drivers' to determine correct "
-                    "extension for {} format. Try using 'GTiff' or 'KEA' "
+                    "extension for {} format. Try using 'ENVI' or 'netCDF' "
                     "instead".format(args.of))
 
     for dataset in args.infiles:
         print("\n** {} **\n".format(os.path.basename(dataset)))
 
-        subdataset_names = get_subdataset_names(dataset)
+        subdataset_names = get_subdataset_names(dataset,
+                                                resolution=args.resolution)
 
         for num, subdataset in enumerate(subdataset_names):
             out_name = get_out_name(subdataset, out_ext)
@@ -147,7 +255,21 @@ if __name__ == "__main__":
             if args.of == "netCDF":
                 gdal_translate_cmd.extend(["-ot", "Int16"])
 
-            gdal_translate_cmd.extend([subdataset, out_file])
+            gdal_translate_cmd.extend([subdataset[0], out_file])
             print(" ".join(gdal_translate_cmd))
             subprocess.check_call(gdal_translate_cmd)
+
+        # Merge QI layers for each granule (GDAL doesn't do this).
+        if args.extract_qi:
+            print(" Extracting QI layers")
+            input_basename = os.path.split(dataset)[1]
+            input_basename = input_basename.replace(".SAFE","")
+            out_qi_dir = os.path.join(args.outdir, input_basename + "_qi")
+            try:
+                os.makedirs(out_qi_dir)
+            except OSError:
+                # Assume already exists and continue
+                pass
+
+            merge_qi_vectors(dataset, out_qi_dir)
 
